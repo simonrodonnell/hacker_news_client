9e4a0a83fd76edbf59cd0e5ec18d76448330a20b
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-12-11 13:21:47 +0000

    Promise.all to stories

--- src/containers/StoryContainer.js
+++ src/containers/StoryContainer.js
@@ -2,32 +2,36 @@ import React from 'react'
 
 class StoryContainer extends React.Component {
 
-constructor(props) {
-  super(props);
-  this.state = {
-    storyURLs: [],
-    stories: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      storyURLs: [],
+      stories: []
+    }
   }
-}
 
-componentDidMount() {
-  const url = "https://hacker-news.firebaseio.com/v0/topstories.json"
-  fetch(url)
-  .then(res => res.json())
-  .then(idList => idList.map((storyID) => {
-    return `https://hacker-news.firebaseio.com/v0/item/${storyID}.json`
+  componentDidMount() {
+    const url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    fetch(url)
+    .then(res => res.json())
+    .then(idList => idList.map((storyID) => {
+      return `https://hacker-news.firebaseio.com/v0/item/${storyID}.json`
     }))
-  .then(urlList => this.setState({ storyURLs: urlList}))
-  .catch(err => console.error);
-}
+    .then(urlList => Promise.all(
+      urlList.map((url) => {
+        return fetch(url)
+        .then(response => response.json())
+      })
+    ))
+    .then(response => this.setState({stories: response}))
+    .catch(err => console.error);
+    }
 
-
-
-render(){
-  return(
-    <h1>Hello!</h1>
-  )
-}
+  render() {
+    return(
+      <h1>Hello!</h1>
+    )
+  }
 
 }
 
